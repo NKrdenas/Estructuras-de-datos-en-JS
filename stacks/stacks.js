7409ef808883b41be03bba2b089b8979e1c72c52
NKrdenas <ncardenas101903@gmail.com>
2023-05-25 16:02:38 -0500

    Stack y sus metodos

---
+++ stacks/stacks.js
@@ -0,0 +1,41 @@
+class Node{
+    constructor(value){
+        this.value = value
+        this.next = null
+    }
+}
+
+class Stack{
+    constructor(){
+        this.top = null
+        this.bottom = null
+        this.length = 0
+    }
+    peek(){
+        return this.top
+    }
+    push(value){
+        const newNode = new Node(value)
+        if(this.top == null){
+            this.top = newNode
+            this.bottom = newNode
+        }else{
+            const holdingPointer = this.top
+            this.top = newNode
+            this.top.next = holdingPointer
+        }
+        this.length++
+        return this        
+    }
+    pop(){
+        if(this.length > 0){
+            const popped = this.top
+            this.top = this.top.next
+            this.length--
+            return popped
+        }
+        return null
+    }
+}
+
+const myStack = new Stack()
