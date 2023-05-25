48acd23194f36a662844460bd18efda4068a3a23
NKrdenas <ncardenas101903@gmail.com>
2023-05-25 17:21:17 -0500

    BinarySearchTrees y sus respectivos metodos

---
+++ trees/trees.js
@@ -0,0 +1,51 @@
+class Node{
+    constructor(value){
+        this.left = null
+        this.right = null
+        this.value = value
+    }
+}
+
+class BinarySearchTree{
+    constructor(){
+        this.root = null
+    }
+    insert(value){
+        const newNode = new Node(value)
+        if(this.root === null){
+            this.root = newNode
+        }else{
+            let currentNode = this.root
+            while(true){
+                if(value < currentNode.value){
+                    if(!currentNode.left){
+                        currentNode.left = newNode
+                        return this
+                    }
+                    currentNode = currentNode.left
+                    }else if(value > currentNode.value){
+                        if(!currentNode.right){
+                            currentNode.right = newNode
+                            return this
+                        }
+                    currentNode = currentNode.right
+                }
+            }
+        }
+    }
+    search(value){
+        let currentNode = this.root
+        while(currentNode){
+            if(value < currentNode.value){
+                currentNode = currentNode.left
+            }else if(value > currentNode.value){
+                currentNode = currentNode.right
+            }else{
+                return currentNode
+            }
+        }
+        return null
+    }
+}
+
+const tree = new BinarySearchTree()
